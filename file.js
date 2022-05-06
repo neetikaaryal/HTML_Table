f753e119eb7345585cf5a57d783938b582d7b265
Neetika Aryal <neetikaaryal16@gmail.com>
2022-05-06 15:12:55 +0545

    HTML_TABLE

---
+++ file.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.getElementById("demo").style.backgroundColor = "yellow"
+    document.getElementById("demo1").style.backgroundColor = "red"
+    document.getElementById("demo2").style.borderBottomColor = "blue"
+    document.getElementById("demo3").style.borderBottomColor = "blue"
+    }
